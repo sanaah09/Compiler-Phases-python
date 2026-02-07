a2fa3a37585da6e3a9a4baa2e97a6a8a1eb6ceaf
sanaah09 <sanaah0905@gmail.com>
2026-02-07 22:35:48 +0530

    Create antlr_demo.py

---
+++ ai_tool_antlr/antlr_demo.py
@@ -0,0 +1,7 @@
+# Demonstration of using ANTLR-generated lexer and parser
+# (Code generation step is omitted for simplicity)
+
+print("ANTLR is used to generate lexer and parser automatically using grammar rules.")
+print("Grammar file: Expr.g4")
+print("Input example: a = b + 10;")
+print("ANTLR validates syntax based on grammar.")
